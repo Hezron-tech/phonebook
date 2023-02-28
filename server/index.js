d63a9552812340fe618f5b3b365e8e44fd72b398
Hezron-tech <kiprophez@gmail.com>
2023-02-28 12:31:41 +0300

    connecting to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const cors =requires('cors')
+const cors =require('cors')
+const connectDB = require('./config/db');
+connectDB();
 const app=express()
+
+port=process.env.PORT||3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port,()=>{
+    console.log('server is running');
+})
+
 
 
 
