e149c2e629a91d00c92659685170a33121e8756a
Hezron-tech <kiprophez@gmail.com>
2023-03-01 18:20:55 +0300

    worked on search button

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
 import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+  
     FormComponent,
     EditComponent
   ],
